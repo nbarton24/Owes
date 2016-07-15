ca832c664e85bd57cb2b98b4e31d7cadae0cb401
Nick Barton <barton_nich@me.com>
2016-07-15 14:57:47 -0500

    Initial Commit

---
+++ WhoOweMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhoOweMe
+//
+//  Created by Nick Barton on 7/15/16.
+//  Copyright © 2016 Nick Barton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
